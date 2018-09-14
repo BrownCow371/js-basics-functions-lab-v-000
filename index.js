aa9892e28f097719f58a91fded1e0fb8de8d5841
Jamie Brown <jmbrowncow@gmail.com>
2018-09-14 01:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function calculatesFarePrice(start, destination) {
   } else if ((distance > 400 ) && (distance <= 2000)) {
     return ((distance - 400) * 0.02);
   } else if ((distance > 2000) && (distance < 2500)) {
-    return = 25;
+    return 25;
   }
   // return = 'cannot travel that far';
 }
